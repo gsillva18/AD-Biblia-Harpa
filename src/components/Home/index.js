f0737a9ef2d71a89f18a1781d392a190de68de3a
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2023-07-03 19:24:35 -0300

    criation page home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function Home() {
   return (
-    <div>
-      Esta é a página home
+    <div id="id-home-componente">
+      <Box>
+        <Stack
+          flexWrap="wrap"
+          justifyContent="center"
+          alignContent="center"
+          alignItems="center"
+          sx={{
+            height: window.innerHeight - 110
+          }}
+        >
+          <Typography variant="h6">
+            "UNIÃO, COMUNHÃO E ORAÇÃO"
+          </Typography>
+          <Typography variant="body2">
+            ESSE É O NOSSO LEMA
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              top: 30,
+              maxWidth: 150
+            }}
+          >
+            SAIBA MAIS
+          </Button>
+        </Stack>
+
+      </Box>
     </div>
   );
 }
